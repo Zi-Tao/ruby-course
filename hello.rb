cc536a22fff360bcdd06734717efd72fd6d26052
Zi-Tao <Immatricolarsi@gmail.com>
2022-07-09 21:25:10 +0800

    add ruby methods

--- hello.rb
+++ hello.rb
@@ -1,8 +1,10 @@
 # Lesson 1: Outputting a String
 
-puts "Hello World!"
+# puts "Hello World!"
+
+puts "What is your name?"
 
 # Lesson 2: Prompting User Input
 
-# name = gets
-# puts "Hey, #{name}! Nice to meet you!"
+name = gets.chop # The chop method takes a string and removes the very last character of that string, in this case, the newline character.
+puts "Hey, #{name}! Nice to meet you!"
